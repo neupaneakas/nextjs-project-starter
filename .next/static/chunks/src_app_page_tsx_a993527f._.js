1654c2c24da11cd42d09fb5ca607efbd4a63e7e5
Aakash Neupane <84524694+neupaneakas@users.noreply.github.com>
2025-08-12 14:13:36 +0545

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1754987311638

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_56b1e159._.js",
+  "static/chunks/node_modules_f8d0e0f0._.js"
+],
+    source: "dynamic"
+});
